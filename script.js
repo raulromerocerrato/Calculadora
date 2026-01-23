262af3b92b97d833ed8a843ec9bfa2d1485a2745
raulromerocerrato <raulromero@paucasesnovescifp.cat>
2026-01-23 09:23:00 +0100

    Update the calculation function without eval()

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-let pantalla=document.getElementById('pantalla');
+let pantalla = document.getElementById('pantalla');
 
-function agregar(valor){
-	pantalla.value+=valor;
+function agregar(valor) {
+	pantalla.value += valor;
 }
 
-function calcular(){
-	try{
-		pantalla.value=eval(pantalla.value);
-		if(pantalla.value=='undefined'){
-			pantalla.value='Expresió mal escrita';
+function calcular() {
+	try {
+		let expresion = pantalla.value;
+		let numeros = expresion.split(/[\+\-\*\/]/).map(Number);
+		let operadores = expresion.match(/[\+\-\*\/]/g);
+		if (operadores==null) return;
+		let resultado = numeros[0];
+		for (let i = 0; i < operadores.length; i++) {
+			switch (operadores[i]) {
+				case '+':
+					resultado += numeros[i + 1];
+					break;
+				case '-':
+					resultado -= numeros[i + 1];
+					break;
+				case '*':
+					resultado *= numeros[i + 1];
+					break;
+				case '/':
+					resultado /= numeros[i + 1];
+					break;
+			}
 		}
-	}catch{
-		pantalla.value="Syntax Error";
+		pantalla.value = resultado;
+	} catch {
+		pantalla.value = "Syntax Error";
 	}
 }
 
-function limpiar(){
-	pantalla.value=' ';
+function limpiar() {
+	pantalla.value = '';
 }
 
-document.addEventListener('keydown',(event)=>{
-	const tecla=event.key;
-	if(/[0-9]/.test(tecla)){
+document.addEventListener('keydown', (event) => {
+	const tecla = event.key;
+
+	if (/[0-9]/.test(tecla)) {
 		agregar(tecla);
-	}else if(['+','-','*','/'].includes(tecla)){
+	} else if (['+', '-', '*', '/'].includes(tecla)) {
 		agregar(tecla);
-	}else if(tecla==='Enter'){
+	} else if (tecla === 'Enter') {
 		calcular();
-	}else if(tecla==='Backspace'){
-		limpiar()
-	}else{
-		alert("Tecla no disponible");
+	} else if (tecla === 'Backspace') {
+		limpiar();
 	}
 });
